1877a842f8d212f400754d2de539fa1ee35237ce
hackerbuddy229 <rasmusb229@gmail.com>
2020-11-19 20:41:14 +0100

    🎉Created Server project

---
+++ Attendance-bot.Net/server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Attendance_bot.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
