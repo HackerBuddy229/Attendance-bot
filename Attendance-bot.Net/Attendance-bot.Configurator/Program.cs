a925c2167a676a09c2cfea289df0553c16b7a7a3
Rasmus Bengtsson <rasmusb229@gmail.com>
2020-11-20 17:40:57 +0100

    🎉Added configurator project to sln

---
+++ Attendance-bot.Net/Attendance-bot.Configurator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Attendance_bot.Configurator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
